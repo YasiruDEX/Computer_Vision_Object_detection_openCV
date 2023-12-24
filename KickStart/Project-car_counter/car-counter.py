538d61c07d75df1c8c1ded031218f7140828f47f
Basnayake B.Y.N <YasiruDEX@users.noreply.github.com>
2023-12-24 20:18:21 +0530

    Update: finalized car counter

---
+++ KickStart/Project-car_counter/car-counter.py
@@ -0,0 +1,104 @@
+from ultralytics import YOLO
+import cv2
+import cvzone
+import math
+from sort import *
+import numpy as np
+
+cap = cv2.VideoCapture("Videos/cars.mp4") #For video
+
+model = YOLO('../Yolo_weights/yolov8n.pt')
+
+classNames = ["person", "bicycle", "car", "motorbike", "aeroplane", "bus", "train", "truck", "boat",
+              "traffic light", "fire hydrant", "stop sign", "parking meter", "bench", "bird", "cat",
+              "dog", "horse", "sheep", "cow", "elephant", "bear", "zebra", "giraffe", "backpack", "umbrella",
+              "handbag", "tie", "suitcase", "frisbee", "skis", "snowboard", "sports ball", "kite", "baseball bat",
+              "baseball glove", "skateboard", "surfboard", "tennis racket", "bottle", "wine glass", "cup",
+              "fork", "knife", "spoon", "bowl", "banana", "apple", "sandwich", "orange", "broccoli",
+              "carrot", "hot dog", "pizza", "donut", "cake", "chair", "sofa", "pottedplant", "bed",
+              "diningtable", "toilet", "tvmonitor", "laptop", "mouse", "remote", "keyboard", "cell phone",
+              "microwave", "oven", "toaster", "sink", "refrigerator", "book", "clock", "vase", "scissors",
+              "teddy bear", "hair drier", "toothbrush"
+              ]
+
+mask = cv2.imread("Project-car_counter/mask.png")
+
+#Tracking
+tracker = Sort(max_age=20, min_hits=3, iou_threshold=0.3)
+
+limits = [400,297,673,297]
+
+total_count = []
+
+while True:
+    success, img = cap.read()
+    imgRegion = cv2.bitwise_and(img, mask)
+
+    imgGraphics = cv2.imread("Project-car_counter/graphics.png",cv2.IMREAD_UNCHANGED)
+    img = cvzone.overlayPNG(img,imgGraphics,(0,0))
+
+    results = model(imgRegion,stream=True)
+
+    detections = np.empty((0,5))
+
+    for r in results:
+        boxes = r.boxes
+        for box in boxes:
+            
+            #Bounding box
+            x1,y1,x2,y2 = box.xyxy[0]
+            # x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
+            # print(x1, y1, x2, y2)
+            # cv2.rectangle(img, (x1,y1,x2,y2), (255,0,255), 3)
+            
+            # x1,y1,w,h = box.xywh[0]
+            bbox = int(x1), int(y1), int(x2-x1), int(y2-y1)
+
+            #confidence
+            conf = math.ceil(box.conf[0]*100)/100            
+            print(conf)
+
+            #Class Name
+            cls = int(box.cls[0])
+            # print(cls)
+
+            CurrentClass = classNames[cls]
+
+            if (CurrentClass == "car" or CurrentClass == "bus" or CurrentClass == "motorbike" 
+                or CurrentClass == "truck") and conf > 0.3:
+                # cvzone.putTextRect(img, f"{CurrentClass} {conf}", ( max(0,int(x1)), max(40,int(y1)) ), 
+                #                    scale=0.6, thickness=1, offset = 3)
+                # cvzone.cornerRect(img, bbox,l=10,rt=5)
+                currentArray = np.array([int(x1),int(y1),int(x2),int(y2),conf])
+                detections = np.vstack((detections, currentArray))
+
+    resultsTracker = tracker.update(detections)
+
+    cv2.line(img,(limits[0], limits[1]), (limits[2],limits[3]),(0,0,255),5)
+
+    for result in resultsTracker:
+        x1, y1, x2, y2, id = result            
+        print(result)
+        bbox = int(x1), int(y1), int(x2-x1), int(y2-y1)
+        cvzone.cornerRect(img, bbox, l=9, rt = 2, colorR=(255,0,255))
+        cvzone.putTextRect(img, f"{id}", ( max(0,int(x1)), max(40,int(y1)) ), 
+                                   scale=2, thickness=3, offset = 10)
+        
+        cx, cy = int(x1)+int(x2-x1)//2, int(y1)+int(y2-y1)//2
+        cv2.circle(img, (cx,cy),5,(255,0,255),cv2.FILLED)
+
+        if limits[0] < cx < limits[2] and limits[1] - 15 < cy < limits[1] + 15:
+            if total_count.count(id) == 0:
+                total_count.append(id)
+                cv2.line(img,(limits[0], limits[1]), (limits[2],limits[3]),(0,255,0),5)
+
+
+    # cvzone.putTextRect(img, f"Count: {len(total_count)}", ( 50,50 ))
+                
+    cv2.putText(img, str(len(total_count)), (240,100), cv2.FONT_HERSHEY_PLAIN, 5, (50,50,255),8)
+        
+
+    cv2.imshow("Image", img)
+    # cv2.imshow("ImageRegion", imgRegion)
+    cv2.waitKey(1)
+
